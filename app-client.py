e39dca84bf31fe7a7a378e0440f4e13fd9b17fa3
Tetimaru <Tetimaru@users.noreply.github.com>
2019-03-29 15:14:57 -0700

    Split message class in libserver to better represent what it does and moved functions unrelated to socket read/write to main server class

--- app-client.py
+++ app-client.py
@@ -6,14 +6,20 @@ import libclient
 # should get host and port from the command line
 HOST = '127.0.0.1'
 PORT = 65432
+EVENTS = selectors.EVENT_READ | selectors.EVENT_WRITE
 
-if len(sys.argv) < 3:
-    print("usage: ./app-server.py <host> <port>")
-    sys.exit()
+# if len(sys.argv) < 3:
+    # print("usage: ./app-server.py <host> <port>")
+    # sys.exit()
     
-host = sys.argv[1]
-port = sys.argv[2]
+# host = sys.argv[1]
+# port = sys.argv[2]
 sel = selectors.DefaultSelector()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# global flags
+request_queued = False # only make a new message class for write event if request_queued & done_processing_msg
+done_processing_msg = False # if true, make a new message class for next event
 
 request = {
     "function": "lock",
@@ -23,24 +29,35 @@ request = {
     }
 }
 
-print(sys.argv[0], sys.argv[1], request)
-
+def create_request(request):
+    message = libclient.Message(sel, sock, (HOST, PORT), request)
+    sel.register(sock, EVENTS, data=message)
+    global request_queued 
+    request_queued = True
             
 def start_connection(host, port, request):
     addr = (host, port)
     print('starting connection to', addr)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
     sock.connect_ex(addr)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    message = libclient.Message(sel, sock, addr)
-    message.add_new_request(request)
-    sel.register(sock, events, data=message)
-    
+    create_request(request)
+
+def main():
+    start_connection(HOST, PORT, request)
     while True:
         events = sel.select(timeout=None)
         for key, mask in events:
-            message.process_events(mask)
+            if mask & selectors.EVENT_READ:
+                pass
+                #self.read()
+            if mask & selectors.EVENT_WRITE:
+            # check if we have queued a new request 
+            # if yes, create a new message class
+                if request_queued:
+                    # key.data should hold the message class 
+                    message = key.data 
+                    message.process_events(mask)
     
 if __name__ == '__main__':
-    start_connection(HOST, PORT, request)
+    main()
+    
--- libserver.py
+++ libserver.py
@@ -4,113 +4,25 @@ import struct
 import json
 import random
 
-class Message:
-    def __init__(self, sel, conn, addr): # constructor
+class MessageOut:
+    def __init__(self, sel, conn, addr, request): # constructor
         self.sel = sel 
         self.sock = conn 
         self.addr = addr
-        self._recv_buffer = b''
         self._send_buffer = b''
-        self._jsonheader_len = 0
-        self.jsonheader = None
-        self.request = None
+        self.outgoing_request = request
         self.response_created = False
-    
-    # fixed-length header
-    # 2-byte integer containing length of JSON header
-    def process_protoheader(self):
-        hdrlen = 2 
-        if len(self._recv_buffer) >= hdrlen:
-            self._jsonheader_len = struct.unpack('>H', self._recv_buffer[:hdrlen])[0]
-            self._recv_buffer = self._recv_buffer[hdrlen:]
-            
-    def process_jsonheader(self):
-        hdrlen = self._jsonheader_len
-        if len(self._recv_buffer) > hdrlen:
-            #self.jsonheader = self._json_decode(self._recv_buffer[hdrlen], 'utf-8')
-            self._recv_buffer = self._recv_buffer[hdrlen:]
-            for reqhdr in ('byteorder', 'content-length', 'content-type', 'content-encoding'):
-                if reqhdr not in self.jsonheader:
-                    raise ValueError(f'Missing required header "{reqhdr}".')
-    
-    def color(self, player, x, y, fill):
-        pass
-        
-    # player, x, y: int 
-    # lock grid(x,y) for player
-    def lock(self, player, x, y):
-        print("locking grid(", x, y, ") for player", player)
-        
-    def isLocked(self, x, y):
-        print(x, y)
-        return True if (random.random() > 0.5) else False
-    
+         
     # serialize data (python dict) into bytes
     def _json_encode(self, data):
         return json.dumps(data).encode('utf-8')
 
-    # deserialize bytes into python dict
-    def _json_decode(self, data):
-        return json.loads(data)
-                        
-    def process_request(self):
-        #content_len = self.jsonheader['content-length']
-        #if not len(self._recv_buffer) >= content_len:
-            #return
-        if not self._recv_buffer:
-            return
-            
-        self.request = self._json_decode(self._recv_buffer) # dict form
-        print('received request', repr(self.request), 'from', self.addr)
-        #print(locals()["self"])
-        if self.request["function"] == "lock":
-            #self.request_type = 
-            isLocked = self.isLocked(self.request["args"]["x"], self.request["args"]["y"])
-            if isLocked:
-                pass # block
-            else: # can continue
-                # send response
-                pass 
-                #print(getattr(self, self.request["function"])(*self.request["arg_list"])) # call function
+    def create_response(self):
+        message = self._json_encode(self.outgoing_request)
+        self.response_created = True
+        self._send_buffer += message
+        # send messages to all the other clients
         
-        self._recv_buffer = b''
-
-        # we want to write a response, so monitor for write availability 
-        self.sel.modify(self.sock, selectors.EVENT_WRITE, data=self)
-
-    def process_events(self, mask):
-        if mask & selectors.EVENT_READ:
-            self.read()
-        if mask & selectors.EVENT_WRITE:
-            self.write()
-
-    # receive data from socket and store in _recv_buffer
-    def _read(self):
-        try:
-            data = self.sock.recv(4096) # up to 4096 bytes
-            #print(repr(data))
-        except BlockingIOError: # Resource temporarily unavailable (errno EWOULDBLOCK)
-            pass
-        else:
-            if data:
-                self._recv_buffer += data
-            else:
-                raise RuntimeError("Peer closed.") # ???
-    
-    # entry point when data is available to be read on socket
-    def read(self):
-        self._read()
-        
-        #if self._jsonheader_len is None:
-            #self.process_protoheader()
-        #else:
-            #if self.jsonheader is None:
-                #self.process_jsonheader()
-                
-        #if self.jsonheader:
-            #if self.request is None:
-        self.process_request()
-    
     def _write(self):
         if self._send_buffer:
             print('sending', repr(self._send_buffer), 'to', self.addr)
@@ -124,36 +36,65 @@ class Message:
                 # when buffer has drained, monitor for read events again
                 if sent and not self._send_buffer:
                     self.sel.modify(self.sock, selectors.EVENT_READ, data=self)
-    
-    def create_response(self):
-        if self.request["function"] == "lock":
-            # check if the requested grid is already locked or coloured
-            args = self.request["args"]
-            isLocked = self.isLocked(args["x"], args["y"])
-            if isLocked: # block
-                response = {
-                    "function": "lock",
-                    "success": 0
-                }
-            else: # can lock, update board state
-                self.lock(1, args["x"], args["y"])
-                response = {
-                    "function": "lock",
-                    "success": 1
-                }
-
-        message = self._json_encode(response)
-        self.response_created = True
-        self._send_buffer += message
         
     def write(self):
-        if self.request:
-            if not self.response_created:
-                self.create_response()
+        if not self.response_created:
+            self.create_response()
                 
         self._write()
+
+
+class MessageIn:
+    def __init__(self, sel, conn, addr): # constructor
+        self.sel = sel 
+        self.sock = conn 
+        self.addr = addr
+        self._recv_buffer = b''
+        self.client_request = None
+        self.response_created = False
+    
+    def color(self, player, x, y, fill):
+        pass
         
-    def close(self):
-        print('closing connection to', self.addr)
-        self.sel.unregister(self.sock)
-        self.sock.close()
+    # player, x, y: int 
+    # lock grid(x,y) for player
+    def lock(self, player, x, y):
+        print("locking grid(", x, y, ") for player", player)
+
+    # deserialize bytes into python dict
+    def _json_decode(self, data):
+        return json.loads(data)
+                        
+    def process_read_buffer(self):
+        if not self._recv_buffer:
+            return
+           
+        self.client_request = self._json_decode(self._recv_buffer) # dict form
+        print('received request', repr(self.client_request), 'from', self.addr)
+
+        # we want to write a response, so monitor for write availability 
+        self.sel.modify(self.sock, selectors.EVENT_WRITE, data=self)
+
+    # receive data from socket and store in _recv_buffer
+    def _read(self):
+        try:
+            data = self.sock.recv(4096) # up to 4096 bytes
+            #print(repr(data))
+        except BlockingIOError: # Resource temporarily unavailable (errno EWOULDBLOCK)
+            pass
+        else:
+            if data:
+                self._recv_buffer += data
+            else:
+                raise RuntimeError("server _read: received \
+                no data (shouldn't be here?)") # shouldn't happen on server side
+    
+    # entry point when data is available to be read on socket
+    def read(self):
+        self._read()
+        self.process_read_buffer()
+
+        if self.client_request:
+        	return self.client_request
+    
+
