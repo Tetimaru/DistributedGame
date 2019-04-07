4c69b5f5322ac0bba073fb8b7682d077cd5bb631
Charles Tsui <chtsui@sfu.ca>
2019-04-07 16:50:28 -0700

    other players locking square will not reset game board

--- app-server.py
+++ app-server.py
@@ -14,8 +14,8 @@ gameBoard = Board.createBoard(8,8)
 
 # should get host and port from the command line
 
-#HOST = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-HOST = '127.0.0.1'
+HOST = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+# HOST = '127.0.0.1'
 PORT = 65432
 
 ALL_COLORS = [ (255, 0, 0), # red
